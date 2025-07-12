161d35e4301f3314a3a3374db7fa38052319ce01
SagDev <sagdev122@gmail.com>
2025-07-12 10:19:51 -0600

    create repository

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Puedes agregar configuraciones de Vite o Astro si quieres, 
+  // pero para Tailwind no necesitas el plugin @tailwindcss/vite
+});
