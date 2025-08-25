5c4f9183f1dc05aeb5eff5de772f2af65d86e116
daridev <darideveloper@gmail.com>
2025-08-25 14:31:34 -0600

    deploy (astro.config.mjs): add site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Puedes agregar configuraciones de Vite o Astro si quieres, 
-  // pero para Tailwind no necesitas el plugin @tailwindcss/vite
+  site: 'https://aft-advanced-graph-generator.apps.darideveloper.com',
+  base: '/',
 });
