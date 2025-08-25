2ce9fa0a0b81d7bef04e3aa0870096eef70cd24a
daridev <darideveloper@gmail.com>
2025-08-25 14:41:06 -0600

    fix (astro.config.mjs): inline styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://aft-advanced-graph-generator.apps.darideveloper.com',
   base: '/',
+  build: {
+    inlineStylesheets: 'always',
+  }
 });
