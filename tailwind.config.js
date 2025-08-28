b8ccf4eb767caee47c0f76daf021518d7b458fb7
daridev <darideveloper@gmail.com>
2025-08-28 14:27:19 -0600

    style (talwind.config.js): update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    "bg-tech-red",
+    "bg-tech-green",
+    "bg-tech-blue",
+    "text-tech-red",
+    "text-tech-green",
+    "text-tech-blue",
+    "border-tech-red",
+    "border-tech-green",
+    "border-tech-blue",
+  ],
   theme: {
     extend: {
       colors: {
         'tech-red': '#fcc47e',
-        'tech-green': '#77d9b8',
+        'tech-green': '#c0d2cc',
         'tech-blue': '#123c55',
       }
     },
