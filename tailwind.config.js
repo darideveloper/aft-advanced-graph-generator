a76276b3c87efb7a1cd960d6525c1c82761ad38a
daridev <darideveloper@gmail.com>
2025-08-29 13:49:28 -0600

    feat (BarChart): update colors and calculate in frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   safelist: [
-    "bg-tech-red",
+    "bg-tech-orange",
     "bg-tech-green",
     "bg-tech-blue",
-    "text-tech-red",
+    "text-tech-orange",
     "text-tech-green",
     "text-tech-blue",
-    "border-tech-red",
+    "border-tech-orange",
     "border-tech-green",
     "border-tech-blue",
   ],
   theme: {
     extend: {
       colors: {
-        'tech-red': '#fcc47e',
+        'tech-orange': '#fcc47e',
         'tech-green': '#c0d2cc',
         'tech-blue': '#123c55',
       }
